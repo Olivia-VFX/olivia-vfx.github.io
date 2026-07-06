4d0802213ce3e24793a59193da5f6600e2cff5ab
Olivia Vann <xerovann860@gmail.com>
2026-07-06 17:47:43 +0100

    Update dotproduct.js

--- javascript/dotproduct.js
+++ javascript/dotproduct.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("canvas");
-canst ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 const origin = { x: canvas.width / 2, y: canvas.height / 2};
 
@@ -17,26 +17,28 @@ function drawArrow(vector, color) {
 
   ctx.strokeStyle = color;
   ctx.fillStyle = color;
-  ctx.lineWodth = 3;
+  ctx.lineWidth = 3;
 
   const angle = Math.atan2(tip.y - origin.y, tip.x - origin.x);
   const headLength = 12;
 
   ctx.beginPath();
   ctx.moveTo(tip.x, tip.y);
+  ctx.lineTo(origin.x, origin.y);
+  ctx.stroke();
   ctx.lineTo(
     tip.x - headLength * Math.cos(angle - Math.PI / 6),
     tip.y - headLength * Math.sin(angle - Math.PI / 6)
     );
   ctx.lineTo(
-    tip/x - headLength * Math.cos(angle + Math.PI / 6),
+    tip.x - headLength * Math.cos(angle + Math.PI / 6),
     tip.y - headLength * Math.sin(angle + Math.PI / 6)
     );
   ctx.closePath();
   ctx.fill();
 }
 
-function() {
+function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawArrow(vectorA, "#ff6b47");
 }
